c5f86f4bbacb969724532ebf17466cf89be71943
DESKTOP-I65T3K3\lenovo <realsteelatomst@gmail.com>
2023-11-27 16:42:46 +0200

    CheckPassword1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+    public class Main {
+        public static void main(String[] args) {
+            Numbers.averageValue(1,2);
+            Numbers.sum(Numbers.averageValue(100,11),Numbers.averageValue(51,40));
+
+            System.out.println("--------------------------");
+
+            Numbers.averageValue(22,103);
+            Numbers.sum(Numbers.averageValue(2,-5),Numbers.averageValue(-50,40));
+
+            System.out.println("--------------------------");
+
+            Numbers.checkIfNumbersPositive(1,2,3);
+            Numbers.checkIfNumbersPositive(-1,2,3);
+            Numbers.checkIfNumbersPositive(-1,-2,3);
+            Numbers.checkIfNumbersPositive(-1,-2,-3);
+
+            System.out.println("--------------------------");
+
+            Numbers.checkIfNumbersEqual(2,3,5);
+            Numbers.checkIfNumbersEqual(2,2,5);
+            Numbers.checkIfNumbersEqual(2,2,2);
+            Numbers.checkIfNumbersEqual(-2,2,-5);
+            Numbers.checkIfNumbersEqual(-5,-5,-5);
+
+            System.out.println("--------------------------");
+
+            CheckPassword fromConsole = new CheckPassword();
+            CheckPassword lessThanEightSymbols = new CheckPassword("kkk");
+            CheckPassword onlyLetters = new CheckPassword("abcdefghj");
+            CheckPassword onlyNumbers = new CheckPassword("123456789");
+            CheckPassword onlySpecialSymbols = new CheckPassword("!@#$%^&*(");
+            CheckPassword correctPassword = new CheckPassword("pass123!@");
+
+            fromConsole.acceptFromConsole();
+            fromConsole.checkPassword(fromConsole.passwordAccepted);
+            System.out.println("-------------------");
+
+            lessThanEightSymbols.checkPassword(lessThanEightSymbols.passwordAccepted);
+            System.out.println("-------------------");
+
+            onlyLetters.checkPassword(onlyLetters.passwordAccepted);
+            System.out.println("-------------------");
+
+            onlyNumbers.checkPassword(onlyNumbers.passwordAccepted);
+            System.out.println("-------------------");
+
+            onlySpecialSymbols.checkPassword(onlySpecialSymbols.passwordAccepted);
+            System.out.println("-------------------");
+
+            correctPassword.checkPassword(correctPassword.passwordAccepted);
+            System.out.println("-------------------");
+        }
+}
---
+++ src/main/java/org/example/Numbers.java
@@ -0,0 +1,40 @@
+package org.example;
+
+
+    public class Numbers {
+        public static void checkIfNumbersPositive (int a, int b, int c){
+            if (a >= 0 && b >= 0 && c >= 0){
+                System.out.println("All numbers are positive.");
+            }
+            else {
+                System.out.println("There are negative numbers!");
+            }
+        }
+        public static void checkIfNumbersEqual(int a, int b, int c){
+            if(a == b){
+                if (b == c){
+                    System.out.printf("Numbers %d, %d and %d are equal.\n", a, b, c);
+                }
+            }
+            else {
+                System.out.printf("Numbers %d, %d and %d are not equal.\n", a, b, c);
+            }
+        }
+        public static double averageValue(double a, double b){
+            double result = (a + b) / 2;
+            System.out.printf("The average value of %.1f and %.1f is: %.1f\n",a, b, result);
+            return result;
+        }
+        public static double sum(double a, double b){
+            double result = a + b;
+            System.out.printf("The sum of %.1f and %.1f is: %.1f\n",a, b, result);
+            return result;
+        }
+
+    /*
+    На последните два метода, ако махна реда за принтиране и оставя само return result,
+    в мейн метода ми казва Result of 'Numbers.sum()' is ignored.
+    Единственото решение, което намерих е да сложа резултата в променлива, която да използвам.
+    Ако може коментар дали и как това може да се избегне и съответно да принтирам само числата.
+     */
+}
